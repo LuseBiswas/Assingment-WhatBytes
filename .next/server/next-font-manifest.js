ad540fe55690224c49bc86ef5986e627c0a3a2d7
Ritesh Biswas <lusebiswas@gmail.com>
2024-11-16 20:52:02 +0530

    next deploy

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/riteshbiswas/Desktop/whatbytes-frontend-main/src/app/layout":["static/media/a34f9d1faa5f3315-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"XWyAFnLVn76ZVu6SS2FuAksLl1+ISJhIzGcbtD07USo=\"}"
